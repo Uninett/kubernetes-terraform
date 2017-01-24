2878ed531181e7b2d3b7a236abd7a7c1284f10db
Olav Morken <olav.morken@uninett.no>
2017-01-24 10:30:48 +0100

    Add script & config for generating certificates.
    This patch adds a script and some config for generating certificates.

---
+++ ansible/tls/generate_cert.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [ $# -lt 3 ]; then
+    echo 'Usage: generate_cert.sh CA PROFILE NAME [ALTNAMES...]' >&2
+    exit 1
+fi
+CA="$1"; shift
+PROFILE="$1"; shift
+NAME="$1" # No shift here, because we are going to use $@ to get all names later
+
+# From http://stackoverflow.com/a/17841619
+function join_by {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
+NAMES="$(join_by ',' $@)"
+
+if [ ! -f "${CA}-ca.json" ]; then
+    echo "Invalid CA: ${CA}" >&2
+    exit 1
+fi
+
+
+if [ -f "output/${NAME}.names" ]; then
+    if [ "$(<"output/${NAME}.names")" = "${NAMES}" ]; then
+	echo "Certificate for ${NAME} already exists -- skipping" >&2
+	exit 0
+    fi
+fi
+
+if [ ! -x .cfssljson ]; then
+    echo "Fetching cfssl binaries" >&2
+    if [ "$(uname -sp)" = "Linux x86_64" ]; then
+	variant=linux-amd64
+    elif [ "$(uname -sp)" = "Darwin i386" ]; then
+	variant=darwin-amd64
+    else
+	echo "Unknown OS variant: $(uname -sp)" >&2
+	exit 1
+    fi
+    curl -sSL -o .cfssl "https://pkg.cfssl.org/R1.2/cfssl_${variant}"
+    chmod +x .cfssl
+    curl -sSL -o .cfssljson "https://pkg.cfssl.org/R1.2/cfssljson_${variant}"
+    chmod +x .cfssljson
+fi
+
+if [ ! -d 'output' ]; then
+    mkdir 'output'
+fi
+
+if [ ! -f "output/${CA}-ca.pem" ]; then
+    echo "Generating CA certificate & key for ${CA}" >&2
+    ./.cfssl gencert -initca "${CA}-ca.json" | ./.cfssljson -bare "output/${CA}-ca"
+fi
+
+# From http://stackoverflow.com/a/8088167/1954565
+define(){
+    IFS='\n'
+    read -r -d '' ${1} || true
+}
+
+define REQUEST <<EOF
+{
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "CN": "${NAME}"
+}
+EOF
+
+echo "$REQUEST" | ./.cfssl gencert -ca="output/${CA}-ca.pem" -ca-key="output/${CA}-ca-key.pem" -config=ca-config.json -profile="${PROFILE}" -hostname="${NAMES}" - | ./.cfssljson -bare "output/${NAME}"
+echo "${NAMES}" >"output/${NAME}.names"
