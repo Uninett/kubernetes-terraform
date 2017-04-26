75b31950b2258ba73bdd93a0954c67d63c9a38ee
Morten Knutsen <morten.knutsen@uninett.no>
2017-04-26 14:16:50 +0200

    Work around strange issues with creation of sec group rules.
    Make sure we create them serialy.

--- run_dev.sh
+++ run_dev.sh
@@ -8,6 +8,20 @@ if [ ! -f terraform/local.tfvars ]; then
 fi
 
 pushd terraform
+# This first (serial) run of the security group rules are needed due to strange
+# behaviour where only some of the rules are there after the initial run unless
+# the are processed sequentially.
+#
+# See:
+#     https://github.com/hashicorp/terraform/issues/7519
+#
+terraform apply -var-file ipnett.tfvars -var-file local.tfvars -parallelism 1 \
+          -target openstack_networking_secgroup_rule_v2.ssh_access_ipv4 \
+          -target openstack_networking_secgroup_rule_v2.kube_lb_http_ipv4 \
+          -target openstack_networking_secgroup_rule_v2.kube_lb_https_ipv4 \
+          -target openstack_networking_secgroup_rule_v2.kube_master_ipv4
+
+# Now, do the rest in parallell as normal
 terraform apply -var-file ipnett.tfvars -var-file local.tfvars
 terraform output inventory >../ansible/inventory
 popd
