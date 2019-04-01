aa403fe5a18b04b1b6867d4712f6acc89fbd01bf
Hogne Håskjold <hogne.haskjold@uninett.no>
2019-04-01 14:26:51 +0200

    Add support for Ansible environments
    Trying to follow the structure in https://docs.ansible.com/ansible/latest/user_guide/playbooks_best_practices.html#alternative-directory-layout
    
    Two environments added. One for Safespring and one for a test installation with AWS EKS. Note that the latter is only tested with the prometheus_operator role as teh Terraform parts and a lot of the other ansible roles don't apply to a managed Kubernetes solution.

--- run_dev.sh
+++ run_dev.sh
@@ -2,6 +2,32 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+usage() { echo "Usage: $0 -e <ANSIBLE_ENVIRONMENT>"; exit 0; }
+
+while getopts e: option
+do
+    case "${option}" in
+	e)
+	    ANSIBLE_ENV=${OPTARG}
+	    ;;
+	*)
+            usage
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+if [ -z "${ANSIBLE_ENV}" ] ; then
+    usage
+fi
+
+ANSIBLE_ENV_DIR="ansible/environments/${ANSIBLE_ENV}"
+
+if [ ! -d "${ANSIBLE_ENV_DIR}" ]; then
+    echo "The Ansible environment you specificed doesn't seem to exist, please create it at: ${ANSIBLE_ENV_DIR}" >&2
+    exit 1
+fi
+
 if [ ! -f terraform/local.tfvars ]; then
     echo "You must create terraform/local.tfvars" >&2
     exit 1
@@ -23,7 +49,7 @@ terraform apply -var-file ipnett.tfvars -var-file local.tfvars -parallelism=1 \
 
 # Now, do the rest in parallell as normal
 terraform apply -var-file ipnett.tfvars -var-file local.tfvars
-terraform output inventory >../ansible/inventory
+terraform output inventory >"../${ANSIBLE_ENV_DIR}"/hosts
 popd
 
-./ansible/apply.sh
+./ansible/apply.sh -e "${ANSIBLE_ENV}"
